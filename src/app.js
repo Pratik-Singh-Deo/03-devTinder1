7021212e37aa8e2dcbc5f979b655b19e27c45aa0
Pratik-Singh-Deo <pratiksinghdeo@gmail.com>
2024-11-30 07:06:39 +0530

    created a server

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/hello", (req, res) => {
+  res.send("Heloo hello hello");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Server is Running");
+});
+
+app.use("/", (req, res) => {
+  res.send("Namaste NodeJs");
+});
+
+app.listen(7777, () => {
+  console.log("Server is running on port 7777...");
+});
